855ffbbd0d1a8d529155119f3aa879ef73cc7ba6
Botond Lovász <botilovasz@gmail.com>
2024-10-02 22:14:41 +0200

    test functions

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use serde::Deserialize;
 use serde_json;
 use std::mem;
 use fxhash::FxHashMap;
+use std::time::Instant;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 #[derive(Serialize, Deserialize, Clone)]
 struct TrieNode {
@@ -76,6 +79,28 @@ impl NGramTrie {
         self.root.insert(n_gram);
     }
 
+    fn merge(&mut self, other: &NGramTrie) {
+        self.root.merge(&other.root);
+    }
+
+    fn size_in_ram(&self) -> usize {
+        mem::size_of::<NGramTrie>() + self.root.size_in_ram()
+    }
+
+    fn save(&self, filename: &str) -> std::io::Result<()> {
+        let file = File::create(filename)?;
+        let writer = BufWriter::new(file);
+        serialize_into(writer, self).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        Ok(())
+    }
+
+    fn load(filename: &str) -> std::io::Result<Self> {
+        let file = File::open(filename)?;
+        let reader = BufReader::new(file);
+        let trie: NGramTrie = deserialize_from(reader).map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
+        Ok(trie)
+    }
+
     fn _preprocess_rule_context(&self, tokens: &[u32], rule_context: Option<&str>) -> Vec<Option<u32>> {
         let mut result = Vec::new();
         
@@ -126,65 +151,29 @@ impl NGramTrie {
         _search(&self.root, &search_context, 0)
     }
 
-    fn save(&self, filename: &str) -> std::io::Result<()> {
-        let file = File::create(filename)?;
-        let writer = BufWriter::new(file);
-        serialize_into(writer, self).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
-        Ok(())
-    }
-
-    fn load(filename: &str) -> std::io::Result<Self> {
-        let file = File::open(filename)?;
-        let reader = BufReader::new(file);
-        let trie: NGramTrie = deserialize_from(reader).map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
-        Ok(trie)
-    }
-
-    fn fit(tokens: &[u32], n_gram_max_length: u32) -> Self {
+    fn fit(tokens: Arc<Vec<u32>>, n_gram_max_length: u32, max_tokens: Option<usize>) -> Self {
         let mut trie = NGramTrie::new(n_gram_max_length);
-        for i in 0..tokens.len() - n_gram_max_length as usize + 1 {
+        let max_tokens = max_tokens.unwrap_or(tokens.len()).min(tokens.len());
+        for i in 0..max_tokens - n_gram_max_length as usize + 1 {
             let n_gram = &tokens[i..i + n_gram_max_length as usize];
             trie.insert(n_gram);
         }
         trie
     }
 
-    fn fit_multithreaded(tokens: &[u32], n_gram_max_length: u32) -> Self {
+    fn fit_multithreaded(tokens: Arc<Vec<u32>>, ranges: Vec<Range<usize>>, n_gram_max_length: u32) -> Self {
         let mut trie = NGramTrie::new(n_gram_max_length);
-
-        let num_threads = std::thread::available_parallelism().map(|p| p.get()).unwrap_or(1);
-        println!("Using {} threads for multithreaded fitting", num_threads);
-
-        let chunk_size = (tokens.len() as f64 / num_threads as f64).ceil() as usize;
-
-        let mut chunks: Vec<Vec<u32>> = Vec::new();
-
-        for i in 0..num_threads {
-            //println!("Processing chunk {}", i);
-            let start = i * chunk_size;
-            let end = if i == num_threads - 1 {
-                tokens.len()
-            } else {
-                (i + 1) * chunk_size + n_gram_max_length as usize - 1
-            };
-            chunks.push(tokens[start..end].to_owned());
-        }
-
-        for chunk in &mut chunks {
-            chunk.shrink_to_fit();
-        }
-        chunks.shrink_to_fit();
-
-        //println!("Number of chunks: {}", chunks.len());
 
         let mut handles = vec![];
 
-        for chunk in chunks {
+        for range in ranges {
             let mut trie_clone = trie.clone();
 
+            let _tokens = tokens.clone();
+
             let handle = std::thread::spawn(move || {
-                for i in 0..chunk.len() - n_gram_max_length as usize + 1 {
-                    let n_gram = &chunk[i..i + n_gram_max_length as usize];
+                for i in range.start..range.end - n_gram_max_length as usize + 1 {
+                    let n_gram = &_tokens[i..i + n_gram_max_length as usize];
                     trie_clone.insert(n_gram);
                 }
                 trie_clone
@@ -200,163 +189,121 @@ impl NGramTrie {
         trie
     }
 
-    fn multithred_recursively(chunks: Arc<Vec<Vec<u32>>>, range: Range<usize>, n_gram_max_length: u32) -> NGramTrie {
-        if range.len() > 1 {
-            let mid = (range.start + range.end) / 2;
-            let left = range.start..mid;
-            let right = mid..range.end;
+    fn fit_multithreaded_recursively(tokens: Arc<Vec<u32>>, ranges: Vec<Range<usize>>, n_gram_max_length: u32) -> Self {
+        if ranges.len() > 1 {
+            let mid = ranges.len() / 2;
+            let left = ranges[..mid].to_vec();
+            let right = ranges[mid..].to_vec();
             // Recursively process both halves
-            let right_clone = chunks.clone();
+            let right_clone = tokens.clone();
             let handle = std::thread::spawn(move || {
-                NGramTrie::multithred_recursively(right_clone, right, n_gram_max_length)
+                NGramTrie::fit_multithreaded_recursively(right_clone, right, n_gram_max_length)
             });
-            let mut left_trie = NGramTrie::multithred_recursively(chunks, left, n_gram_max_length);
+            let mut left_trie = NGramTrie::fit_multithreaded_recursively(tokens, left, n_gram_max_length);
             let right_trie = handle.join().unwrap();
             left_trie.merge(&right_trie);
             left_trie
         } else {
             let mut trie = NGramTrie::new(n_gram_max_length);
-            for i in 0..chunks[range.start].len() - n_gram_max_length as usize + 1 {
-                let n_gram = &chunks[range.start][i..i + n_gram_max_length as usize];
+            let range = &ranges[0];
+            for i in range.start..range.end - n_gram_max_length as usize + 1 {
+                let n_gram = &tokens[i..i + n_gram_max_length as usize];
                 trie.insert(n_gram);
             }
             trie
         }
     }
 
-    fn load_and_split_huge_file(file_path: &str, n_gram_max_length: u32) -> Result<Arc<Vec<Vec<u32>>>, std::io::Error> {
-        // Load the file
-        let file = File::open(file_path)?;
+    fn load_json(filename: &str, max_tokens: Option<usize>) -> std::io::Result<Arc<Vec<u32>>> {
+        let file = File::open(filename)?;
         let reader = BufReader::new(file);
-        let numbers: Vec<u32> = serde_json::from_reader(reader)
-            .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
+        let start = std::time::Instant::now();
+        let mut tokens: Vec<u32> = serde_json::from_reader(reader).map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
+        let duration = start.elapsed();
+        println!("Time taken to load tokens: {:?}", duration);
+        println!("Size of tokens in RAM: {} bytes", tokens.len() * std::mem::size_of::<u32>());
+        if let Some(max) = max_tokens {
+            if max < tokens.len() {
+                tokens.truncate(max);
+            }
+        }
+        println!("Size of tokens in RAM after truncation: {} bytes", tokens.len() * std::mem::size_of::<u32>());
+        Ok(Arc::new(tokens))
+    }
     
-        // Get the number of CPU cores
-        let num_cores = std::thread::available_parallelism()
-            .map(|p| p.get()).unwrap_or(1);
-    
-        // Calculate chunk size
-        let chunk_size = ((numbers.len() as f64) / (num_cores as f64)).ceil() as usize;
-    
-        // Split into chunks
-        let mut chunks: Vec<Vec<u32>> = Vec::new();
-
-        for i in 0..num_cores {
-            //println!("Processing chunk {}", i);
+    fn split_into_ranges(tokens: Arc<Vec<u32>>, max_tokens: usize, num_threads: usize, n_gram_max_length: u32) -> Vec<Range<usize>> {
+        let mut ranges = Vec::new();
+        let max_tokens = std::cmp::min(max_tokens, tokens.len());
+        let chunk_size = (max_tokens as f64 / num_threads as f64).ceil() as usize;
+        for i in 0..num_threads {
             let start = i * chunk_size;
-            let end = if i == num_cores - 1 {
-                numbers.len()
+            let end = if i == num_threads - 1 {
+                max_tokens
             } else {
                 (i + 1) * chunk_size + n_gram_max_length as usize - 1
             };
-            chunks.push(numbers[start..end].to_owned());
+            ranges.push(start..end);
         }
-    
-        // Wrap in Arc and return
-        Ok(Arc::new(chunks))
+        ranges
     }
 
-    /// Merges another trie into this one
-    fn merge(&mut self, other: &NGramTrie) {
-        self.root.merge(&other.root);
-    }
-
-    fn size_in_ram(&self) -> usize {
-        mem::size_of::<NGramTrie>() + self.root.size_in_ram()
-    }
-    
-    
 }
 
-fn load_tokens_from_json(filename: &str) -> std::io::Result<Vec<u32>> {
-    let file = File::open(filename)?;
-    let reader = BufReader::new(file);
-    let tokens: Vec<u32> = serde_json::from_reader(reader).map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
-    Ok(tokens)
+fn test_performance_and_write_stats(tokens: Arc<Vec<u32>>, data_sizes: Vec<usize>, n_gram_lengths: Vec<u32>, output_file: &str) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(output_file)
+        .unwrap();
+
+    writeln!(file, "Data Size,N-gram Length,Fit Time (ms),RAM Usage (MB)").unwrap();
+
+    let num_threads = std::thread::available_parallelism()
+        .map(|p| p.get()).unwrap_or(1);
+
+    for data_size in data_sizes {
+        for n_gram_length in &n_gram_lengths {
+            //let ranges = NGramTrie::split_into_ranges(tokens.clone(), data_size, num_threads, *n_gram_length);
+            // Measure fit time
+            let start = Instant::now();
+            //let trie = NGramTrie::fit_multithreaded(tokens.clone(), ranges, *n_gram_length);
+            //let trie = NGramTrie::fit_multithreaded_recursively(tokens.clone(), ranges, *n_gram_length);
+            let trie = NGramTrie::fit(tokens.clone(), *n_gram_length, Some(data_size));
+            let fit_time = start.elapsed().as_secs_f64(); 
+            // Measure RAM usage
+            let ram_usage = trie.size_in_ram() as f64 / (1024.0 * 1024.0);
+
+            // Write statistics to file
+            writeln!(
+                file,
+                "{},{},{},{:.2}",
+                data_size, n_gram_length, fit_time, ram_usage
+            ).unwrap();
+
+            println!(
+                "Completed: Data Size = {}, N-gram Length = {}, Fit Time = {}, RAM Usage = {:.2} MB",
+                data_size, n_gram_length, fit_time, ram_usage
+            );
+        }
+    }
+}
+
+fn run_performance_tests(filename: &str) {
+    let tokens = NGramTrie::load_json(filename, Some(100_000_000)).unwrap();
+    println!("Tokens loaded: {}", tokens.len());
+    let data_sizes = (1..10).map(|x| x * 1_000_000).chain((1..=10).map(|x| x * 10_000_000)).collect::<Vec<_>>();
+    let n_gram_lengths = [3, 4, 5, 6, 7].to_vec();
+    let output_file = "fit_performance.csv";
+
+    test_performance_and_write_stats(tokens, data_sizes, n_gram_lengths, output_file);
 }
 
 fn main() {
-    let filename = "/home/boti/Desktop/ngram-llm-analysis/data/170k_small_tokenized_data.json";
-    let n_gram_max_length = 30;
+    let filename = "/home/boti/Desktop/ngram-llm-analysis/data/cleaned_tokenized_data.json";
+    run_performance_tests(filename);
 
-    // {   // old approach (single threaded)
-    //     println!("Old approach (single threaded):");
-    //     let tokens = load_tokens_from_json(filename).unwrap();
-    //     println!("Loaded {} tokens from JSON", tokens.len());
-
-    //     let start = std::time::Instant::now();
-    //     let trie = NGramTrie::fit(&tokens, n_gram_max_length);
-    //     let duration = start.elapsed();
-    //     println!("Time taken to fit NGramTrie: {:?}", duration);
-
-    //     let start = std::time::Instant::now();
-    //     let result1 = trie.search(&[4038, 2193, 2332], Some("+++"));
-    //     let duration1 = start.elapsed();
-    //     println!("Result for '+++': {:?}", result1);
-    //     println!("Time taken for '+++': {:?}", duration1);
-
-    //     let start = std::time::Instant::now();
-    //     let result2 = trie.search(&[4038, 2193, 2332], Some("+*+"));
-    //     let duration2 = start.elapsed();
-    //     println!("Result for '+*+': {:?}", result2);
-    //     println!("Time taken for '+*+': {:?}", duration2);
-
-    //     let total_size = trie.size_in_ram();
-    //     println!("Total size of the trie (including all nodes): {:.2} MB", total_size as f64 / (1024.0 * 1024.0));
-    // }
-
-    {   // old approach (multithreaded)
-        println!("Old approach (multithreaded):");
-        let tokens = load_tokens_from_json(filename).unwrap();
-        println!("Loaded {} tokens from JSON", tokens.len());
-
-        let start = std::time::Instant::now();
-        let trie = NGramTrie::fit_multithreaded(&tokens, n_gram_max_length);
-        let duration = start.elapsed();
-        println!("Time taken to fit NGramTrie: {:?}", duration);
-
-        println!("Number of children: {}", trie.root.children.capacity());
-
-        let start = std::time::Instant::now();
-        let result1 = trie.search(&[4038, 2193, 2332], Some("+++"));
-        let duration1 = start.elapsed();
-        println!("Result for '+++': {:?}", result1);
-        println!("Time taken for '+++': {:?}", duration1);
-
-        let start = std::time::Instant::now();
-        let result2 = trie.search(&[4038, 2193, 2332], Some("++*"));
-        let duration2 = start.elapsed();
-        println!("Result for '++*': {:?}", result2);
-        println!("Time taken for '++*': {:?}", duration2);
-
-        let total_size = trie.size_in_ram();
-        println!("Total size of the trie (including all nodes): {:.2} MB", total_size as f64 / (1024.0 * 1024.0));
-    }
-
-    {   // new approach (multithreaded and recursively) 
-        println!("New approach (multithreaded and recursively):");
-        let chunks = NGramTrie::load_and_split_huge_file(filename, n_gram_max_length).unwrap();
-        let chunks_len = chunks.len();
-        println!("Loaded {} chunks from JSON", chunks_len);
-    
-        let start = std::time::Instant::now();
-        let trie = NGramTrie::multithred_recursively(chunks, 0..chunks_len, n_gram_max_length);
-        let duration = start.elapsed();
-        println!("Time taken to fit NGramTrie: {:?}", duration);
-
-        let start = std::time::Instant::now();
-        let result1 = trie.search(&[4038, 2193, 2332], Some("+++"));
-        let duration1 = start.elapsed();
-        println!("Result for '+++': {:?}", result1);
-        println!("Time taken for '+++': {:?}", duration1);
-
-        let start = std::time::Instant::now();
-        let result2 = trie.search(&[4038, 2193, 2332], Some("++*"));
-        let duration2 = start.elapsed();
-        println!("Result for '++*': {:?}", result2);
-        println!("Time taken for '++*': {:?}", duration2);
-
-        let total_size = trie.size_in_ram();
-        println!("Total size of the trie (including all nodes): {:.2} MB", total_size as f64 / (1024.0 * 1024.0));
-    }
+    let x = 371710322;
+    let y = 0.0015 * (x as f64).powf(0.8891) / 1024.0;
+    println!("{}", y);
 }
